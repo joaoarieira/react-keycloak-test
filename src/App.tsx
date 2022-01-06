ee881d5c42af29de5978c92924c2d76de3494a9f
João Azevedo <joao.azevedo@ufv.br>
2022-01-06 11:31:06 -0300

    react-keycloak warnings issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useKeycloak } from '@react-keycloak/web';
 
 function App() {
+  console.log(useKeycloak);
   return (
     <div className="App">
       <header className="App-header">
